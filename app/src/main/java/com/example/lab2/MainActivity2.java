fef65917af75e06e616b678a9790fc77c136961a
david34Y <dgonzalesn@pucp.edu.pe>
2023-04-24 10:48:11 -0500

    commit detalle evento

---
+++ app/src/main/java/com/example/lab2/MainActivity2.java
@@ -0,0 +1,40 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toolbar;
+
+import java.util.Objects;
+
+public class MainActivity2 extends AppCompatActivity {
+    private ImageButton backButton;
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        backButton = findViewById(R.id.imageButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btn = findViewById(R.id.button3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/lab2/MainActivity3.java
@@ -0,0 +1,24 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity3 extends AppCompatActivity {
+    private ImageButton backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        backButton = findViewById(R.id.imageButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
